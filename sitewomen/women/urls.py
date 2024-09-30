537a59f7295d6765618733a3fba2a94140116d0a
Salavat <salavatgumerov14@gmail.com>
2024-09-30 19:10:08 +0300

    Динамические URL. Пользовательские конвертеры

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
 
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories),
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('archive/<year4:year>/', views.archive),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,7 +6,16 @@ def index(request):
     return HttpResponse('Страница приложения women')
 
 
-def categories(request):
-    return HttpResponse('Страница с категориями')
+def categories(request, cat_id):
+    return HttpResponse(f'Статьи по категориям, id {cat_id}')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'Статьи по категориям, slug {cat_slug}')
+
+
+def archive(request, year):
+    return HttpResponse(f'Статьи по категориям, год {year}')
+
 
 
