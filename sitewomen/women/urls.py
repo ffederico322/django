0daf27abcdaae6e23a4767a0f009c0e8534de0e1
Salavat <salavatgumerov14@gmail.com>
2024-09-30 22:23:08 +0300

    Введение в шаблоны (Django Template Language, DTL)

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -7,6 +7,7 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('cats/<int:cat_id>/', views.categories, name = 'cats_id'),
     path('cats/<slug:cat_slug>/', views.categories_by_slug, name = 'cats'),
     path('archive/<year4:year>/', views.archive, name = 'archive'),
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 def index(request):
-    return HttpResponse('Страница приложения women')
+    return render(request, 'women/index.html')
+
+
+def about(request):
+    return render(request, 'women/about.html')
 
 
 def categories(request, cat_id):
