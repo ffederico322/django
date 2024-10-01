029d3c82b21874a97814e17d21e294cff20676c6
Salavat <salavatgumerov14@gmail.com>
2024-10-01 15:00:00 +0300

    Теги шаблонов. Теги if и for

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,11 +7,20 @@ from django.template.loader import render_to_string
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+
 def index(request):
     data = {
         'title': 'Главная страница',
         'menu': menu,
+        'posts': list(filter(lambda x: x['is_published'], data_db)),
     }
+
     return render(request, 'women/index.html', context=data)
 
 
