d854cfd7c507b1c3de0572c9e92160b05bfd1c63
Salavat <salavatgumerov14@gmail.com>
2024-10-03 13:12:23 +0300

    Создание и запуск файлов миграций

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -7,3 +7,4 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
